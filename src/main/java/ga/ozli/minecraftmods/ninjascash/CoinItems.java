e32a6acd8ed9c1fa91e38df3b2e82a22fbbf3ed1
Oscar N <PaintNinja@users.noreply.github.com>
2021-09-04 22:44:33 +0100

    Port to 1.17

--- src/main/java/ga/ozli/minecraftmods/ninjascash/CoinItems.java
+++ src/main/java/ga/ozli/minecraftmods/ninjascash/CoinItems.java
@@ -1,17 +1,17 @@
 package ga.ozli.minecraftmods.ninjascash;
 
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
-import net.minecraftforge.fml.RegistryObject;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.fmllegacy.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public final class CoinItems {
 
-    public static final ItemGroup COINS_GROUP = new ItemGroup("ninjascash.coins") {
+    public static final CreativeModeTab COINS_GROUP = new CreativeModeTab("ninjascash.coins") {
         @Override
-        public ItemStack createIcon() {
+        public ItemStack makeIcon() {
             return new ItemStack(TWO_POUNDS.get());
         }
     };
@@ -19,12 +19,12 @@ public final class CoinItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, NinjasCash.MOD_ID);
 
     public static final RegistryObject<Item>
-            ONE_PENCE = ITEMS.register("one_pence", () -> new Item(new Item.Properties().group(COINS_GROUP))),
-            TWO_PENCE = ITEMS.register("two_pence", () -> new Item(new Item.Properties().group(COINS_GROUP))),
-            FIVE_PENCE = ITEMS.register("five_pence", () -> new Item(new Item.Properties().group(COINS_GROUP))),
-            TEN_PENCE = ITEMS.register("ten_pence", () -> new Item(new Item.Properties().group(COINS_GROUP))),
-            TWENTY_PENCE = ITEMS.register("twenty_pence", () -> new Item(new Item.Properties().group(COINS_GROUP))),
-            FIFTY_PENCE = ITEMS.register("fifty_pence", () -> new Item(new Item.Properties().group(COINS_GROUP))),
-            ONE_POUND = ITEMS.register("one_pound", () -> new Item(new Item.Properties().group(COINS_GROUP))),
-            TWO_POUNDS = ITEMS.register("two_pounds", () -> new Item(new Item.Properties().group(COINS_GROUP)));
+            ONE_PENCE = ITEMS.register("one_pence", () -> new Item(new Item.Properties().tab(COINS_GROUP))),
+            TWO_PENCE = ITEMS.register("two_pence", () -> new Item(new Item.Properties().tab(COINS_GROUP))),
+            FIVE_PENCE = ITEMS.register("five_pence", () -> new Item(new Item.Properties().tab(COINS_GROUP))),
+            TEN_PENCE = ITEMS.register("ten_pence", () -> new Item(new Item.Properties().tab(COINS_GROUP))),
+            TWENTY_PENCE = ITEMS.register("twenty_pence", () -> new Item(new Item.Properties().tab(COINS_GROUP))),
+            FIFTY_PENCE = ITEMS.register("fifty_pence", () -> new Item(new Item.Properties().tab(COINS_GROUP))),
+            ONE_POUND = ITEMS.register("one_pound", () -> new Item(new Item.Properties().tab(COINS_GROUP))),
+            TWO_POUNDS = ITEMS.register("two_pounds", () -> new Item(new Item.Properties().tab(COINS_GROUP)));
 }
--- src/main/java/ga/ozli/minecraftmods/ninjascash/NoteItems.java
+++ src/main/java/ga/ozli/minecraftmods/ninjascash/NoteItems.java
@@ -1,17 +1,17 @@
 package ga.ozli.minecraftmods.ninjascash;
 
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
-import net.minecraftforge.fml.RegistryObject;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.fmllegacy.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public final class NoteItems {
 
-    public static final ItemGroup NOTES_GROUP = new ItemGroup("ninjascash.notes") {
+    public static final CreativeModeTab NOTES_GROUP = new CreativeModeTab("ninjascash.notes") {
         @Override
-        public ItemStack createIcon() {
+        public ItemStack makeIcon() {
             return new ItemStack(TWENTY_NOTE.get());
         }
     };
@@ -19,10 +19,10 @@ public final class NoteItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, NinjasCash.MOD_ID);
 
     public static final RegistryObject<Item>
-            FIVE_NOTE = ITEMS.register("five_note", () -> new Item(new Item.Properties().group(NOTES_GROUP))),
-            TEN_NOTE = ITEMS.register("ten_note", () -> new Item(new Item.Properties().group(NOTES_GROUP))),
-            TWENTY_NOTE = ITEMS.register("twenty_note", () -> new Item(new Item.Properties().group(NOTES_GROUP))),
-            FIFTY_NOTE = ITEMS.register("fifty_note", () -> new Item(new Item.Properties().group(NOTES_GROUP))),
-            HUNDRED_NOTE = ITEMS.register("hundred_note", () -> new Item(new Item.Properties().group(NOTES_GROUP))),
-            FORGED_NOTE = ITEMS.register("forged_note", () -> new Item(new Item.Properties().group(NOTES_GROUP)));
+            FIVE_NOTE = ITEMS.register("five_note", () -> new Item(new Item.Properties().tab(NOTES_GROUP))),
+            TEN_NOTE = ITEMS.register("ten_note", () -> new Item(new Item.Properties().tab(NOTES_GROUP))),
+            TWENTY_NOTE = ITEMS.register("twenty_note", () -> new Item(new Item.Properties().tab(NOTES_GROUP))),
+            FIFTY_NOTE = ITEMS.register("fifty_note", () -> new Item(new Item.Properties().tab(NOTES_GROUP))),
+            HUNDRED_NOTE = ITEMS.register("hundred_note", () -> new Item(new Item.Properties().tab(NOTES_GROUP))),
+            FORGED_NOTE = ITEMS.register("forged_note", () -> new Item(new Item.Properties().tab(NOTES_GROUP)));
 }
