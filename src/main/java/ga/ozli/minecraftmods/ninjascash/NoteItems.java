6b898e6481ea908ff1999717f424003fd1e0ef1b
Paint_Ninja <PaintNinja@users.noreply.github.com>
2021-12-01 08:49:24 +0000

    1.18 port

--- src/main/java/ga/ozli/minecraftmods/ninjascash/NoteItems.java
+++ src/main/java/ga/ozli/minecraftmods/ninjascash/NoteItems.java
@@ -3,7 +3,7 @@ package ga.ozli.minecraftmods.ninjascash;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.fmllegacy.RegistryObject;
+import net.minecraftforge.registries.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
